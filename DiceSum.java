c6b1e5ab53c130a0f26cfeceada7dba8fe9f4d9e
FlandereKua <quan3ba@gmail.com>
2023-05-13 14:01:58 +0700

    FINALLYYYY

--- DiceSum.java
+++ DiceSum.java
@@ -1,10 +1,10 @@
 public class DiceSum extends DieRoll {
-    private DieRoll r1;
+    private DieRoll r1;//remove the "//"
     private DieRoll r2;
     public DiceSum(DieRoll r1,DieRoll r2) {
-	super(0,0,0);
+	super(0,0,0); //add another 0
 	this.r1=r1;
-	this.r2=r2;
+	this.r2=r2; //add another "."
     }
     public RollResult makeRoll() {
 	return r1.makeRoll().andThen(r2.makeRoll());
